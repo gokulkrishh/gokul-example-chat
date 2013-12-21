5aff017e2bce9d185a846a757fad5906183eaae5
gokulkrishh <krishnangokul9@gmail.com>
2013-12-21 13:29:33 +0530

    added route function to chat

--- Chat/api/controllers/MainController.js
+++ Chat/api/controllers/MainController.js
@@ -42,36 +42,51 @@ var MainController =
              }
         });
     }
-         
-    },
+},
     login: function (req, res) 
     {
-         login: function (req, res) {
-    var username = req.param("username");
-    var password = req.param("password");
+         login: function (req, res) 
+         {
+            var username = req.param("username");
+            var password = req.param("password");
      
-    Users.findByUsername(username).done(function(err, usr) {
-        if (err) {
-            res.send(500, { error: "DB Error" });
-        } else {
-            if (usr) {
-                var hasher = require("password-hash");  // adding password hash module to variable hasher
-                if (hasher.verify(password, usr.password)) { //verifying the password with hasher
-                    req.session.user = usr;
-                    res.send(usr);
-                } else {
-                    res.send(400, { error: "Wrong Password" });
+            Users.findByUsername(username).done(function(err, usr) {
+            if (err) 
+            {
+                res.send(500, { error: "DB Error" });
+            } 
+            else 
+            {
+                if (usr) 
+                {
+                    var hasher = require("password-hash");  // adding password hash module to variable hasher
+                    if (hasher.verify(password, usr.password)) { //verifying the password with hasher
+                        req.session.user = usr;
+                        res.send(usr);
+                    } 
+                    else 
+                    {
+                        res.send(400, { error: "Wrong Password" });
+                    }
+                } 
+                else 
+                {
+                    res.send(404, { error: "User not Found" });
                 }
-            } else {
-                res.send(404, { error: "User not Found" });
             }
-        }
-    });
-}
-    },
+        });
+    }
+},
     chat: function (req, res) 
     {
-         
+         if (req.session.user) 
+         {
+            res.view({username: req.session.user.username}); //checking user is logged in or not and if logged, load the view
+         }
+         else // if not logged in redirect to index page
+         {
+            res.redirect('/');
+         }
     }
 };
 module.exports = MainController;  
