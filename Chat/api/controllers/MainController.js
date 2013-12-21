214b3bea53a15a9f427aa207118f9f60a08bcc68
gokulkrishh <krishnangokul9@gmail.com>
2013-12-21 14:01:01 +0530

    modified an error

--- Chat/api/controllers/MainController.js
+++ Chat/api/controllers/MainController.js
@@ -42,37 +42,37 @@ var MainController =
         });
     }
 },
-    login: function (req, res) 
-    {
-            var username = req.param("username");
-            var password = req.param("password");
+login: function (req, res) 
+{
+        var username = req.param("username");
+        var password = req.param("password");
      
-            Users.findByUsername(username).done(function(err, usr) {
-            if (err) 
+        Users.findByUsername(username).done(function(err, usr) {
+        if (err) 
+        {
+            res.send(500, { error: "DB Error" });
+        } 
+        else 
+        {
+            if (usr) 
             {
-                res.send(500, { error: "DB Error" });
-            } 
-            else 
-            {
-                if (usr) 
-                {
-                    var hasher = require("password-hash");  // adding password hash module to variable hasher
-                    if (hasher.verify(password, usr.password)) { //verifying the password with hasher
-                        req.session.user = usr;
-                        res.send(usr);
-                    } 
-                    else 
-                    {
-                        res.send(400, { error: "Wrong Password" });
-                    }
+                var hasher = require("password-hash");  // adding password hash module to variable hasher
+                if (hasher.verify(password, usr.password)) //verifying the password with hasher
+                {                               
+                    req.session.user = usr;
+                    res.send(usr);
                 } 
                 else 
                 {
-                    res.send(404, { error: "User not Found" });
+                    res.send(400, { error: "Wrong Password" });
                 }
+            } 
+            else 
+            {
+                    res.send(404, { error: "User not Found" });
             }
-        });
-    }
+        }
+    });
 },
     chat: function (req, res) 
     {
