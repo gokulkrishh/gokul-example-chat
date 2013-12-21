4bd1496514e7a0d3c42c1270d92deeba811ea537
gokulkrishh <krishnangokul9@gmail.com>
2013-12-21 12:11:31 +0530

    created funz for each route and view is rendered in index function

--- Chat/api/controllers/MainController.js
+++ Chat/api/controllers/MainController.js
@@ -1,30 +1,23 @@
-/**
- * MainController
- *
- * @module      :: Controller
- * @description	:: A set of functions called `actions`.
- *
- *                 Actions contain code telling Sails how to respond to a certain type of request.
- *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
- *
- *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
- *                 and/or override them with custom routes (`config/routes.js`)
- *
- *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
- *
- * @docs        :: http://sailsjs.org/#!documentation/controllers
- */
+// creating function for each route in MainController
 
-module.exports = {
-    
-  
+var MainController = 
+{
+    index: function (req, res)  // req for http request and res for http response
+    {
+         res.view(); //Views are UI of app and which uses embedded javascript(ejs) files and that views are rendered here
+    },
+    signup: function (req, res) 
+    {
 
-
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to MainController)
-   */
-  _config: {}
-
-  
+         
+    },
+    login: function (req, res) 
+    {
+         
+    },
+    chat: function (req, res) 
+    {
+         
+    }
 };
+module.exports = MainController;  
