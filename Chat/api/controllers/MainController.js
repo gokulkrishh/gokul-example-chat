19870b02b465466801876a99d9990ccfc687fb45
gokulkrishh <krishnangokul9@gmail.com>
2013-12-21 14:08:11 +0530

    modified an error

--- Chat/api/controllers/MainController.js
+++ Chat/api/controllers/MainController.js
@@ -1,14 +1,13 @@
 // creating function for each route in MainController
 
-
 var MainController = 
 {
-    index: function (req, res)  // req for http request and res for http response
-    {
-         res.view(); //Views are UI of app and which uses embedded javascript(ejs) files and that views are rendered here
-    },
-    signup: function (req, res) 
-    {
+index: function (req, res)  // req for http request and res for http response
+{
+    res.view(); //Views are UI of app and which uses embedded javascript(ejs) files and that views are rendered here
+},
+signup: function (req, res) 
+{
         var username = req.param("username"); //getting username from post request
         var password = req.param("password"); //getting pwd from post request
          
@@ -40,25 +39,24 @@ var MainController =
                 });
              }
         });
-    }
 },
 login: function (req, res) 
 {
-        var username = req.param("username");
-        var password = req.param("password");
+    var username = req.param("username");
+    var password = req.param("password");
      
-        Users.findByUsername(username).done(function(err, usr) {
+    Users.findByUsername(username).done(function(err, usr) 
+    {
         if (err) 
         {
             res.send(500, { error: "DB Error" });
         } 
         else 
         {
-            if (usr) 
-            {
-                var hasher = require("password-hash");  // adding password hash module to variable hasher
-                if (hasher.verify(password, usr.password)) //verifying the password with hasher
-                {                               
+            if (usr) {
+                var hasher = require("password-hash");
+                if (hasher.verify(password, usr.password)) //verifying the password with same password module
+                { 
                     req.session.user = usr;
                     res.send(usr);
                 } 
@@ -69,12 +67,12 @@ login: function (req, res)
             } 
             else 
             {
-                    res.send(404, { error: "User not Found" });
+                res.send(404, { error: "User not Found" });
             }
         }
     });
 },
-    chat: function (req, res) 
+chat: function (req, res) 
     {
          if (req.session.user) 
          {
